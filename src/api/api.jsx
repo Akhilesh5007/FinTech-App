fc2785408810c0fe4847df9d03687abf02cc2dc7
Akhilesh Kumar Gupta <50178782+Akhilesh5007@users.noreply.github.com>
2025-09-13 09:11:12 +0530

    Headers removed

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,8 +1,15 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000", // json-server root
+//   headers: { "Content-Type": "application/json" }
+// });
+
+// export default api;
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // json-server root
-  headers: { "Content-Type": "application/json" }
+  baseURL: "https://json-server-api-skp6.onrender.com"
 });
 
 export default api;
